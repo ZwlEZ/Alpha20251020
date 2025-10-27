aac402a99daa8de9092a145f9b2002d13ded51da
ZwlLoveCHN1949101 <159230239+ZwlEZ@users.noreply.github.com>
2025-10-27 16:08:44 +0800

    25.10.27

--- resource.h
+++ resource.h
@@ -3,14 +3,18 @@
 // π© Alpha20251020.rc  π”√
 //
 #define IDI_ICON                        101
-#define IDB_BITMAP1                     102
 #define IDI_ICON1                       103
+#define IDB_AIR                         104
+#define IDB_PERSONA                     105
+#define IDB_POINTS_                     106
+#define IDB_WALLS                       107
+#define IDB_BOX                         108
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
--- winmain.c
+++ winmain.c
@@ -3,20 +3,52 @@
 #include "resource.h"
 
 // 变量声明
-HINSTANCE hIns;
-HBITMAP hBmp = NULL;
-HDC hdc, bmpdc;
-int score = 0;
-HWND hScoreLabel = NULL;
+HINSTANCE hinstance = 0;
+HDC hdc = 0;
+
+// 位图和设备上下文声明
+HBITMAP airBmp = 0, wallBmp = 0, boxBmp = 0, pointBmp = 0, personaBmp = 0;
+HDC airDc = 0, wallDc = 0, boxDc = 0, pointDc = 0, personaDc = 0;
+
+// 关卡数据声明
+int levelSelection = 0;
+struct LEVEL
+{
+	unsigned char personaX, personaY;   // 人物坐标
+	unsigned char pointsX, pointsY;     // 目标点坐标
+	unsigned char pointsX_2, pointsY_2; // 第二个目标点坐标
+	unsigned char scene[10][10];        // 场景数组
+
+}template[] = {
+    5, 5,
+    2, 2,
+    7, 7,
+    {
+        1,1,1,1,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,1,
+        1,0,2,0,0,0,0,0,0,1,
+        1,0,0,0,0,0,0,0,0,1,
+        1,0,0,0,0,0,0,0,0,1,
+        1,0,0,0,0,3,0,0,0,1,
+        1,0,0,0,3,0,0,0,0,1,
+        1,0,0,0,0,0,0,2,0,1,
+        1,0,0,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,1,1,1,1
+    },
+
+};
+enum { AIR, WALL, BOX, POINTS_, PERSONA };
 
 // 窗口过程函数声明
 LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
-void OnButtonClick(HWND, LPARAM);
+void LoadResources(HWND);
+void DrawScene(HWND, HDC);
+void ReleaseResources(void);
 
 // 程序入口点
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-	hIns = hInstance;// 保存实例句柄
+    hinstance = hInstance;// 保存实例句柄
 	// 定义并初始化窗口类
     WNDCLASSEX wcex = { 0 };
     // 对结构体各成员初始化
@@ -27,19 +59,28 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
     wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1)); 	// 自定义图标
     wcex.hCursor = LoadCursor(NULL, IDC_ARROW);   	                // 光标：使用标准箭头光标
     wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); 	            // 背景：使用系统窗口颜色
-    wcex.lpszClassName = L"MyWindow";      			                // 窗口类名
+    wcex.lpszClassName = L"Sokoban";      			                // 窗口类名
 	wcex.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1)); // 自定义小图标
 	// 注册窗口类
     if (!RegisterClassEx(&wcex))
         return 0;
+    // 获取屏幕宽高
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+    // 设置窗口宽高
+    int winWidth = 976;
+    int winHeight = 999;
+    // 计算居中位置
+    int x = (screenWidth - winWidth) / 2;
+    int y = (screenHeight - winHeight) / 2;
 	// 创建窗口
 	HWND hWnd = CreateWindowEx(
             0,                              // 扩展样式
-            L"MyWindow",                    // 窗口类名
-            L"你好！Windows",               // 窗口标题
+            L"Sokoban",                     // 窗口类名
+            L"推箱子",                      // 窗口标题
             WS_OVERLAPPEDWINDOW,            // 窗口风格
-            CW_USEDEFAULT, CW_USEDEFAULT,   // 初始位置
-            800, 600,                       // 初始大小
+            x, y,                           // 初始位置
+            winWidth, winHeight,            // 初始大小
             NULL,                           // 父窗口句柄
             NULL,                           // 菜单句柄
             hInstance,                      // 实例句柄
@@ -65,18 +106,18 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT mSgId, WPARAM wParam, LPARAM lParam)
     switch (mSgId)//函数的第二个参数
     {
     case WM_CREATE:// 窗口创建时的初始化代码
-    {
-        OnButtonClick(hWnd, lParam);
-    }
+        
+		LoadResources(hWnd);
         return 0;
 
     case WM_PAINT:// 绘制窗口内容
     {
         PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps);
+        hdc = BeginPaint(hWnd, &ps);
         // 在此处添加绘图代码
-
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+        DrawScene(hWnd, hdc);
+
 		EndPaint(hWnd, &ps);
     }
         return 0;
@@ -110,49 +151,108 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT mSgId, WPARAM wParam, LPARAM lParam)
 	return 0;
 
     case WM_COMMAND:// 处理菜单和控件命令
-    {
-        int wmId = LOWORD(wParam);
-        switch (wmId)
-        {
-        case 1: // 按钮ID
-            score++;
-            // 更新分数显示
-            wchar_t buf[32];
-            wsprintf(buf, L"分数: %d", score);
-            SetWindowText(hScoreLabel, buf);
-            return 0;
-        default:
-            return DefWindowProc(hWnd, mSgId, wParam, lParam);
-        }
-    }
+  
         return 0;
 
     case WM_DESTROY:// 窗口销毁时的清理代码
-        
+		ReleaseResources();
         PostQuitMessage(0);
+
         return 0;
     }
     // 其它未处理的消息交给默认窗口过程
     return DefWindowProc(hWnd, mSgId, wParam, lParam);
 }
 
-// 按钮函数
-void OnButtonClick(HWND hWnd, LPARAM lParam)
+// 资源加载函数
+void LoadResources(HWND hWnd)
 {
-    static HWND hButton = NULL;
-    if (!hButton) {
-        hButton = CreateWindow(
-            L"BUTTON", L"点击我",
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-            10, 10, 100, 30,
-            hWnd, (HMENU)1, hIns, NULL
-        );
-        // 创建分数显示标签
-        hScoreLabel = CreateWindow(
-            L"STATIC", L"分数: 0",
-            WS_VISIBLE | WS_CHILD,
-            120, 10, 100, 30,
-            hWnd, NULL, hIns, NULL
-        );
-    }
+    // 加载位图资源
+	airBmp = LoadBitmap(hinstance, MAKEINTRESOURCE(IDB_AIR));
+	wallBmp = LoadBitmap(hinstance, MAKEINTRESOURCE(IDB_WALLS));
+	boxBmp = LoadBitmap(hinstance, MAKEINTRESOURCE(IDB_BOX));
+	pointBmp = LoadBitmap(hinstance, MAKEINTRESOURCE(IDB_POINTS_));
+    personaBmp = LoadBitmap(hinstance, MAKEINTRESOURCE(IDB_PERSONA));
+    // 创建内存设备上下文
+    HDC hdc = GetDC(hWnd); // 获取屏幕设备上下文
+    airDc = CreateCompatibleDC(hdc);
+    wallDc = CreateCompatibleDC(hdc);
+    boxDc = CreateCompatibleDC(hdc);
+	pointDc = CreateCompatibleDC(hdc);
+    personaDc = CreateCompatibleDC(hdc);
+    // 将位图选入设备上下文
+    SelectObject(airDc, airBmp);
+    SelectObject(wallDc, wallBmp);
+    SelectObject(boxDc, boxBmp);
+    SelectObject(pointDc, pointBmp);
+    SelectObject(personaDc, personaBmp);
+
+    ReleaseDC(hWnd, hdc); // 释放屏幕设备上下文
+	return;
+}
+// 绘制函数（示例）
+void DrawScene(HWND hWnd, HDC hdc)
+{
+    // 兼容性DC
+	HDC memDc = CreateCompatibleDC(hdc);
+	HBITMAP memBmp = CreateCompatibleBitmap(hdc, 960, 960);
+    SelectObject(memDc, memBmp);
+	// 绘制场景
+	for (int y = 0; y < 10; y++)
+	{
+		for (int x = 0; x < 10; x++)
+		{
+			switch (template[levelSelection].scene[y][x])
+			{
+			case AIR:
+				BitBlt(memDc, x * 96, y * 96, 96, 96, airDc, 0, 0, SRCCOPY);
+				break;
+			case WALL:
+				BitBlt(memDc, x * 96, y * 96, 96, 96, wallDc, 0, 0, SRCCOPY);
+				break;
+			case BOX:
+				BitBlt(memDc, x * 96, y * 96, 96, 96, boxDc, 0, 0, SRCCOPY);
+				break;
+			case POINTS_:
+				BitBlt(memDc, x * 96, y * 96, 96, 96, pointDc, 0, 0, SRCCOPY);
+				break;
+			case PERSONA:
+				BitBlt(memDc, x * 96, y * 96, 96, 96, personaDc, 0, 0, SRCCOPY);
+				break;
+			}
+		}
+	}
+    // 将内存DC内容复制到窗口DC
+	BitBlt(hdc, 0, 0, 960, 960, memDc, 0, 0, SRCCOPY);
+    // 清理
+    DeleteObject(memBmp);
+	DeleteDC(memDc);
+	return;
+}
+// 资源释放函数
+void ReleaseResources(void)
+{
+    // 释放位图资源
+    DeleteObject(airBmp);
+    airBmp = NULL;
+	DeleteObject(wallBmp);
+	wallBmp = NULL;
+	DeleteObject(boxBmp);
+	boxBmp = NULL;
+	DeleteObject(pointBmp);
+	pointBmp = NULL;
+	DeleteObject(personaBmp);
+	personaBmp = NULL;
+    // 删除设备上下文
+    DeleteDC(airDc);
+    airDc = NULL;
+    DeleteDC(wallDc);
+    wallDc = NULL;
+    DeleteDC(boxDc);
+    boxDc = NULL;
+    DeleteDC(pointDc);
+    pointDc = NULL;
+    DeleteDC(personaDc);
+	personaDc = NULL;
+	return;
 }
